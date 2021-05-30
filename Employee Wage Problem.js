0f487db4db3120b4847aa538a123b277610b90d7
ok97 <omkhawshi0@gmail.com>
2021-05-30 14:34:20 +0530

    Added: Employee Wage Per Day

--- Employee Wage Problem.js
+++ Employee Wage Problem.js
@@ -11,10 +11,32 @@ let empCheck=Math.floor(Math.random()*4)%2; // Using random function to identity
 
 if (empCheck==IS_ABSENT) // Using selection statement 
 {
- console.log("Employee Present");   
+    console.log("UC1: Employee Absent ");   
 }
 else
 {
-    console.log("Employee Absent");
+    console.log("UC1: Employee Present");
 }
 
+/*UC2:- Ability to Calculate Daily Employee Wage based on part time or full time work.
+        - Use Math.Random to check No Time, Part Time or Full Time
+        - Assume Part Time is 4 Hrs and Full time is 8 Hrs and per hour wage is $20
+        - Solve Using Switch Statement */
+
+        const IS_PART_TIME=1;  //Constants
+        const IS_FULL_TIME=2;
+        const PART_TIME_HOURS=4;
+        const FULL_TIME_HOURS=8;
+        const WAGE_PER_HOUR=20;
+        let empHours=0;
+        switch(empCheck){
+            case IS_FULL_TIME:
+                empHours=FULL_TIME_HOURS;
+        break;
+        case IS_PART_TIME:
+            empHours=PART_TIME_HOURS;
+            default:
+                break;
+        }
+        let dailyWage = empHours*WAGE_PER_HOUR;
+        console.log("UC2: Wage per day is "+dailyWage);
